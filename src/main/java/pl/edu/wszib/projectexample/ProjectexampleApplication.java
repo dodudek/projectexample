a54e06691d8cf5dd62d0b780788ad531a1e59128
Dominika Anna Dudek <dodudek@student.wszib.edu.pl>
2019-05-12 11:12:52 +0200

    projekt - part 1

---
+++ src/main/java/pl/edu/wszib/projectexample/ProjectexampleApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.projectexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectexampleApplication.class, args);
+    }
+
+}
